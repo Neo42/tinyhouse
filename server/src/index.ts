e350c72528344396fb67e31efbb57bd0d98223e3
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-05-01 17:41:05 +0800

    feat(server): 👽️ add basic api & mock listings

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,23 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import {listings} from './listings'
+
 const app = express()
 const port = 9000
 
+app.use(bodyParser.json())
+
 app.get('/', (_req, res) => res.send('Hello world'))
+
+app.get('/listings', (_req, res) => res.send(listings))
+
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) return res.send(listings.splice(i, 1))
+  }
+  return res.send('failed to delete listing.')
+})
 
 app.listen(port)
 
