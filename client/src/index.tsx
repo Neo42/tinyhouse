466c12c2c6849392ebe205c51f4e21943bc7e79e
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-05-03 22:23:59 +0800

    feat(client): ✨ add user login logic

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
 import {Layout} from 'antd'
+import {Viewer} from 'lib/types'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
@@ -11,22 +12,34 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
-const App = () => (
-  <BrowserRouter>
-    <Layout id="app">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/host" element={<Host />} />
-        <Route path="/listing/:id" element={<Listing />} />
-        <Route path="/listings" element={<Listings />} />
-        <Route path="/listings/:location" element={<Listings />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/user/:id" element={<User />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Layout>
-  </BrowserRouter>
-)
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+}
+
+const App = () => {
+  const [viewer, setViewer] = React.useState<Viewer>(initialViewer)
+
+  return (
+    <BrowserRouter>
+      <Layout id="app">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/host" element={<Host />} />
+          <Route path="/listing/:id" element={<Listing />} />
+          <Route path="/listings" element={<Listings />} />
+          <Route path="/listings/:location" element={<Listings />} />
+          <Route path="/login" element={<Login setViewer={setViewer} />} />
+          <Route path="/user/:id" element={<User />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -1,12 +1,87 @@
-import {Card, Layout, Typography} from 'antd'
+import React from 'react'
+import {useApolloClient, useMutation} from '@apollo/client'
+import {Card, Layout, Spin, Typography} from 'antd'
+import {AUTH_URL} from 'lib/graphql/queries'
+import {LOG_IN, LOG_OUT} from 'lib/graphql/mutations'
+import {AuthUrl as AuthUrlData} from 'lib/graphql/queries/AuthUrl/__generated__/AuthUrl'
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from 'lib/graphql/mutations/LogIn/__generated__/LogIn'
+import {Viewer} from 'lib/types'
 import googleLogo from './assets/google_logo.jpeg'
+import {displayErrorMessage, displaySuccessNotification} from 'lib/utils'
+import {ErrorBanner} from 'lib/components'
+import {Navigate, useNavigate} from 'react-router-dom'
+
+interface Props {
+  setViewer: (viewer: Viewer) => void
+}
 
 const {Content} = Layout
 const {Text, Title} = Typography
 
-export const Login = () => {
+export const Login = ({setViewer}: Props) => {
+  const navigate = useNavigate()
+  const client = useApolloClient()
+  const [logIn, {data: LogInData, loading: logInLoading, error: logInError}] =
+    useMutation<LogInData, LogInVariables>(LOG_IN, {
+      onCompleted: (data) => {
+        if (data && data.logIn) {
+          setViewer(data.logIn)
+          displaySuccessNotification("You've successfully logged in!")
+        }
+      },
+    })
+
+  const logInRef = React.useRef(logIn)
+
+  React.useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code')
+
+    if (code) {
+      logInRef.current({
+        variables: {
+          input: {code},
+        },
+      })
+    }
+  }, [])
+
+  const handleAuthorize = async () => {
+    try {
+      const {data} = await client.query<AuthUrlData>({
+        query: AUTH_URL,
+      })
+
+      window.location.href = data.authUrl
+    } catch (error) {
+      displayErrorMessage(
+        "Sorry! We weren't able to log you in. Please try again later!",
+      )
+    }
+  }
+
+  if (logInLoading) {
+    return (
+      <Content className="log-in">
+        <Spin size="large" tip="Logging you in..." />
+      </Content>
+    )
+  }
+
+  if (LogInData && LogInData.logIn) {
+    const {id: viewerId} = LogInData.logIn
+    return <Navigate to={`/user/${viewerId}`} />
+  }
+
+  const logInErrorBannerElement = logInError ? (
+    <ErrorBanner description="Sorry! We weren't able to log you in. Please try again later!" />
+  ) : null
+
   return (
     <Content className="log-in">
+      {logInErrorBannerElement}
       <Card className="log-in-card">
         <div className="log-in-card__intro">
           <Title level={3} className="log-in-card__intro-title">
@@ -19,7 +94,10 @@ export const Login = () => {
           </Title>
           <Text>Sign in with Google to start booking available rentals!</Text>
         </div>
-        <button type="button" className="log-in-card__google-button">
+        <button
+          type="button"
+          className="log-in-card__google-button"
+          onClick={handleAuthorize}>
           <img
             src={googleLogo}
             alt="Google Logo"
