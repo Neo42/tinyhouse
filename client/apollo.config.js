b0b5f2a7210b40ec6b208499f5127dd978e6a862
Hao Jiang ᛋ <22409868+Neo42@users.noreply.github.com>
2022-05-04 11:51:29 +0800

    feat(client): 🧑‍💻 add apollo config for better development experience

---
+++ client/apollo.config.js
@@ -0,0 +1,9 @@
+// Used for VSCode Apollo GraphQL extension intellisense
+module.exports = {
+  client: {
+    service: {
+      name: 'tiny-house',
+      url: 'http://localhost:9000/api',
+    },
+  },
+}
